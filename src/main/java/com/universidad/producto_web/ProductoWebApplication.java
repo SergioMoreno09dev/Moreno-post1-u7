caacdb08eef8ad790b6626bf1f907d2d5f9be90b
Sergio Andrés Moreno Sánchez <fineas25@gmail.com>
2026-04-18 20:06:59 -0500

    Agregar archivos y crpetas de model, controller y service con sus correspondientes clases, servlets y service

---
+++ src/main/java/com/universidad/producto_web/ProductoWebApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.producto_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoWebApplication.class, args);
+	}
+
+}
